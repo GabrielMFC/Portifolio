583290bc8ac32d4931fd63ff8a1acb69bd559a06
Gabriel Matias <ggezcom991gh@gmail.com>
2023-12-09 20:30:47 -0300

    Criando a interação da interface

--- portifolio.js
+++ portifolio.js
@@ -6,3 +6,20 @@ if(valor == "1"){
 if(valor == "2"){
     usuario.src = "Enzodecostas.png"
 }
+
+let identificador = 0
+let msg = "Seja bem vindo(a) ao meu portifólio, Meu nome é Gabriel."
+function Digitarsejabemvindo() {
+    if (identificador < msg.length) {
+        document.getElementById('texto').innerHTML += msg.charAt(identificador)
+        identificador++
+        setTimeout(Digitarsejabemvindo, 130)
+    }
+}
+function sumir() {
+    if(identificador >= msg.length) {
+        imagem.src = "#"
+    }
+}
+let imagem = document.getElementById("chat")
+Digitarsejabemvindo()
