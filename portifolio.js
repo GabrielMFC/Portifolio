396bafdaab38fea713d9fd2df1dfca68ce073225
Gabriel Matias <ggezcom991gh@gmail.com>
2023-12-10 18:05:19 -0300

    Criando o chat do meu personagem

--- portifolio.js
+++ portifolio.js
@@ -7,19 +7,61 @@ if(valor == "2"){
     usuario.src = "Enzodecostas.png"
 }
 
-let identificador = 0
 let msg = "Seja bem vindo(a) ao meu portifólio, Meu nome é Gabriel."
+
+let msgtopico = "Por qual tópico devemos começar?"
+let msgsobremim = "Sobre mim"
+let msghabilidade = "Habilidades"
+let msgprojetos = "Projetos"
+let sobremimdesc = "Eu sou um homem de 17 anos. Não tenho muita experiência na área de TI ainda, mas estou dando o meu melhor."
+var identificador = 0 /* Apresentação */
+ var identificador2 = 0 /* "Por qual tópico..." */
+
+ var identificador3 = 0 /* Sobre mim */
+ var identificador4 = 0 /* Habilidades */
+ var identificador5 = 0 /* Projetos */
+
+ var identificador6 = 0 /* Descrição do Sobre mim */
+
 function Digitarsejabemvindo() {
     if (identificador < msg.length) {
-        document.getElementById('texto').innerHTML += msg.charAt(identificador)
+        document.getElementById("apresentacao").innerHTML += msg.charAt(identificador)
         identificador++
         setTimeout(Digitarsejabemvindo, 130)
-    }
-}
-function sumir() {
-    if(identificador >= msg.length) {
-        imagem.src = "#"
-    }
-}
+    }else{
+        setTimeout(function() {
+            document.getElementById("apresentacao").innerHTML = ""}, 1500)
+        }
+    } 
+    setTimeout( function topico(){
+        document.getElementById("topico").innerHTML += msgtopico.charAt(identificador2)
+        identificador2++
+        setTimeout(topico, 130)
+     }, 9820)
+
+     setTimeout(function todosTopicos() {
+        document.getElementById("topico").innerHTML = ""
+        document.getElementById("sobremim").innerHTML += msgsobremim.charAt(identificador3)
+        setTimeout( function(){
+        document.getElementById("habilidades").innerHTML += msghabilidade.charAt(identificador4)
+        identificador4++
+    }, 1190)
+        setTimeout(function(){
+        document.getElementById("projetos").innerHTML += msgprojetos.charAt(identificador5) 
+        identificador5++}, 2640)
+        identificador3++
+        setTimeout(todosTopicos, 130)
+     }, 15500)
+     
+     /*
+     document.getElementById("sobremim").addEventListener("click", function descSobremim() {
+        document.getElementById("sobremim").innerHTML = ""
+        document.getElementById("habilidades").innerHTML = ""
+        document.getElementById("projetos").innerHTML = ""
+        document.getElementById("descricaosobremim").innerHTML += sobremimdesc.charAt(identificador6)
+        identificador6++
+        setTimeout(descSobremim, 130)
+     })
+     */
 let imagem = document.getElementById("chat")
 Digitarsejabemvindo()
